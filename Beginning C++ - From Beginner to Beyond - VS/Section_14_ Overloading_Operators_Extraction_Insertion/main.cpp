16af9fc39da8e286a504892720c2fd10fa80dffb
Anthony Vuong <anthony.l.vuong@gmail.com>
2023-11-13 13:26:53 -0800

    Section 14 Operator Overloading Part 6
    Implemented istream and ostream operator overloading friend functions for Mystring class

---
+++ Beginning C++ - From Beginner to Beyond - VS/Section_14_ Overloading_Operators_Extraction_Insertion/main.cpp
@@ -0,0 +1,29 @@
+// Section_14_ Overloading_Operators_Extraction_Insertion.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Mystring.h"
+
+int main()
+{
+    Mystring larry{"Larry"};
+    Mystring moe{"Moe"};
+    Mystring curly;
+
+    std::cout << "Enter the 3rd stooge's first name: ";
+    std::cin >> curly;
+
+    std::cout << std::endl;
+
+    std::cout << "The 3 stooges are: " << larry << ", " << moe << ", and " << curly << std::endl;
+
+    std::cout << "Enter the 3 stooges first names seperated by spaces: ";
+    std::cin >> larry >> moe >> curly;
+
+    std::cout << "Again, the 3 stooges are: " << larry << ", " << moe << ", and " << curly << std::endl;
+
+
+    return 0;
+}
+
+
