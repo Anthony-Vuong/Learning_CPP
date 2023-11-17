e8ce38e2ef026a8ffda73b21cee95135fdd3f20c
Anthony Vuong <anthony.l.vuong@gmail.com>
2023-11-17 12:04:23 -0800

    Section 15 Lecture 186 Virtual Destructors
    Defining class method as virtual to allow derived classes to use their own methods. Account example committed

---
+++ Beginning C++ - From Beginner to Beyond - VS/Section_16_Virtual_Functions/Virtual_function.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+class Account {
+public:
+    //In the base class, by defining that this function is virtual, all derived class will use their own version of this function rather than the base
+    virtual void withdraw(double amount) {
+        std::cout << "In Account::withdraw" << std::endl;
+    }
+
+};
+
+class Savings_Account : public Account {
+public:
+    virtual void withdraw(double amount) {
+        std::cout << "In Savings_Account::withdraw" << std::endl;
+    }
+
+};
+
+class Checkings_Account : public Account {
+public:
+    virtual void withdraw(double amount) {
+        std::cout << "In Checkings_Account::withdraw" << std::endl;
+    }
+
+};
+
+class Trust_Account : public Account {
+public:
+    virtual void withdraw(double amount) {
+        std::cout << "In Trust_Account::withdraw" << std::endl;
+    }
+
+};
+
+
+int main()
+{
+
+    Account* p1 = new Account();
+    Account* p2 = new Savings_Account();
+    Account* p3 = new Checkings_Account();
+    Account* p4 = new Trust_Account();
+
+    p1->withdraw(100);
+    p2->withdraw(100);
+    p3->withdraw(100);
+    p4->withdraw(100);
+
+    delete[] p1;
+    delete[] p2;
+    delete[] p3;
+    delete[] p4;
+
+    return 0;
+    
+}
