6050fedefc1866f80df38f0c2346328009435a62
Anthony Vuong <anthony.l.vuong@gmail.com>
2023-12-04 18:29:15 -0800

    Calculator Project - Simple Test Cases for Google Unit Tests
    Created simple test cases for calculator project - addition, subtraction, multiplication, division

--- Calculator/Calculator_Tests/test.cpp
+++ Calculator/Calculator_Tests/test.cpp
@@ -36,3 +36,99 @@ TEST(TestCase4, TestName) {
 	EXPECT_EQ(n, -2);
 	EXPECT_TRUE(true);
 }
+
+TEST(TestCase5, TestSubtract) {
+
+	SimpleCalculator s;
+	int n = s.subtract(1, 3);
+	EXPECT_EQ(n, 2);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestCase6, TestName) {
+
+	SimpleCalculator s;
+	int n = s.subtract(3, 1);
+	EXPECT_EQ(n, -2);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestCase7, TestName) {
+
+	SimpleCalculator s;
+	int n = s.subtract(1, 3);
+	EXPECT_NE(n, -2);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestCase8, TestName) {
+
+	SimpleCalculator s;
+	int n = s.subtract(1, -3);
+	EXPECT_EQ(n, -4);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestCase9, TestMultiply) {
+
+	SimpleCalculator s;
+	int n = s.multiply(3, 0);
+	EXPECT_EQ(n, 0);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestCase10, TestName) {
+
+	SimpleCalculator s;
+	int n = s.multiply(3, 2);
+	EXPECT_EQ(n, 6);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestCase11, TestName) {
+
+	SimpleCalculator s;
+	int n = s.multiply(-1, 3);
+	EXPECT_NE(n, 3);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestCase12, TestName) {
+
+	SimpleCalculator s;
+	int n = s.multiply(1, -3);
+	EXPECT_EQ(n, -3);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestCase13, TestDivide) {
+
+	SimpleCalculator s;
+	int n = s.divide(9, 3);
+	EXPECT_EQ(n, 3);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestCase14, TestName) {
+
+	SimpleCalculator s;
+	int n = s.divide(-9, 3);
+	EXPECT_EQ(n, -3);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestCase15, TestName) {
+
+	SimpleCalculator s;
+	int n = s.divide(-1, -3);
+	EXPECT_NE(n, -3);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestCase16, TestName) {
+
+	SimpleCalculator s;
+	int n = s.divide(9, -3);
+	EXPECT_EQ(n, -3);
+	EXPECT_TRUE(true);
+}
